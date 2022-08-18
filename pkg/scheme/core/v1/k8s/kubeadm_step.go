71d0e0712c706f0f8c64fdb13689e164cc915103
x893675 <x893675@icloud.com>
2022-08-18 17:07:18 +0800

    add build tags for inject kernel parameter
    Signed-off-by: x893675 <x893675@icloud.com>

--- pkg/scheme/core/v1/k8s/kubeadm_step.go
+++ pkg/scheme/core/v1/k8s/kubeadm_step.go
@@ -883,35 +883,8 @@ func EnvSetupSteps(nodes []v1.StepNode) ([]v1.Step, error) {
 		Action:     v1.ActionInstall,
 		Commands: []v1.Command{
 			{
-				Type: v1.CommandShell,
-				ShellCommand: []string{"/bin/bash", "-c", `
-systemctl stop firewalld || true
-systemctl disable firewalld || true
-setenforce 0
-sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
-modprobe br_netfilter && modprobe nf_conntrack
-cat > /etc/sysctl.d/k8s.conf << EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward=1
-EOF
-cat > /etc/sysctl.conf << EOF
-net.ipv6.conf.all.forwarding=1
-fs.file-max = 100000
-vm.max_map_count=262144
-EOF
-cat > /etc/security/limits.conf << EOF
-#IncreaseMaximumNumberOfFileDescriptors
-* soft nproc 65535
-* hard nproc 65535
-* soft nofile 65535
-* hard nofile 65535
-#IncreaseMaximumNumberOfFileDescriptors
-EOF
-sysctl --system
-sysctl -p
-swapoff -a
-sed -i /swap/d /etc/fstab`},
+				Type:         v1.CommandShell,
+				ShellCommand: []string{"/bin/bash", "-c", nodeScript},
 			},
 		},
 	})
