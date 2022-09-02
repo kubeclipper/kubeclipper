e0ea25bfb209f146028786182ce43da1688481ed
Liucw <66344387+Metrora@users.noreply.github.com>
2022-09-02 15:43:48 +0800

    update cron schedule parse and unit test (#178)
    * fix unit test
    
    Signed-off-by: Liucw <liu.chuwei@99cloud.net>
    
    * update parse month-end cron schedule 2022/9/2 final.
    
    Signed-off-by: Liucw <liu.chuwei@99cloud.net>
    
    Signed-off-by: Liucw <liu.chuwei@99cloud.net>
    Co-authored-by: Liucw <liu.chuwei@99cloud.net>

--- pkg/controller/cronbackupcontroller/controller_test.go
+++ pkg/controller/cronbackupcontroller/controller_test.go
@@ -1,36 +1,108 @@
 package cronbackupcontroller
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
-func Test_parseSchedule(t *testing.T) {
+func TestCronBackupReconciler_parseSchedule(t *testing.T) {
+	type fields struct {
+		Now func() time.Time
+	}
 	type args struct {
 		schedule string
 	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
+	type spec struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}
+
+	var generateIntercalaryYearSpec = func() []spec {
+		specs := make([]spec, 366)
+		date := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+		for i := 0; i < len(specs); i++ {
+			t := date
+			specs[i].fields.Now = func() time.Time {
+				return t
+			}
+			specs[i].args = args{
+				schedule: "0 0 L * *",
+			}
+			specs[i].want = generateWant(366, specs[i].fields.Now().Month())
+			specs[i].name = fmt.Sprintf("day %s", specs[i].fields.Now().String())
+			date = date.AddDate(0, 0, 1)
+		}
+		return specs
+	}
+
+	var generateNormalYearSpec = func() []spec {
+		specs := make([]spec, 365)
+		date := time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)
+		for i := 0; i < len(specs); i++ {
+			t := date
+			specs[i].fields.Now = func() time.Time {
+				return t
+			}
+			specs[i].args = args{
+				schedule: "0 0 L * *",
+			}
+			specs[i].want = generateWant(365, specs[i].fields.Now().Month())
+			specs[i].name = fmt.Sprintf("day %s", specs[i].fields.Now().String())
+			date = date.AddDate(0, 0, 1)
+		}
+		return specs
+	}
+
+	var tests []spec
+	tests = append(tests, generateNormalYearSpec()...)
+	tests = append(tests, generateIntercalaryYearSpec()...)
+	tests = append(tests, []spec{
 		{
-			name: "test parse cron schedule without day of month is 'L'",
+			name: "without day of month synax 'L'",
+			fields: fields{
+				Now: func() time.Time {
+					return time.Now()
+				},
+			},
 			args: args{
 				schedule: "0 0 1 * *",
 			},
 			want: "0 0 1 * *",
 		},
-		// {
-		// 	name: "test parse cron schedule with day of month is 'L'",
-		// 	args: args{
-		// 		schedule: "0 0 L * *",
-		// 	},
-		// 	want: "0 0 31 * *",
-		// },
-	}
+	}...)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := parseSchedule(tt.args.schedule); got != tt.want {
-				t.Errorf("parseSchedule() = %v, want %v", got, tt.want)
+			s := &CronBackupReconciler{
+				Now: tt.fields.Now,
+			}
+			if got := s.parseSchedule(tt.args.schedule); got != tt.want {
+				t.Errorf("CronBackupReconciler.parseSchedule() = %v, want %v, %v", got, tt.want, tt.fields.Now().String())
 			}
 		})
 	}
+}
+
+func generateWant(days int, month time.Month) string {
+	if days == 365 {
+		switch month {
+		case time.January, time.March, time.May, time.July, time.August, time.October, time.December:
+			return "0 0 31 * *"
+		case time.April, time.June, time.September, time.November:
+			return "0 0 30 * *"
+		case time.February:
+			return "0 0 28 * *"
+		}
+	}
+	switch month {
+	case time.January, time.March, time.May, time.July, time.August, time.October, time.December:
+		return "0 0 31 * *"
+	case time.April, time.June, time.September, time.November:
+		return "0 0 30 * *"
+	case time.February:
+		return "0 0 29 * *"
+	}
+	panic("unexpected")
 }
