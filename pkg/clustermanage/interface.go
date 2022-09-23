993493304a5fea8f5f8f02b9ad150a66e6229608
lixd <xueduan.li@gmail.com>
2022-09-23 13:16:44 +0800

    feat: add provider precheck api
    Signed-off-by: lixd <xueduan.li@gmail.com>

--- pkg/clustermanage/interface.go
+++ pkg/clustermanage/interface.go
@@ -1,8 +1,19 @@
 package clustermanage
 
-import "context"
+import (
+	"context"
+
+	v1 "github.com/kubeclipper/kubeclipper/pkg/scheme/core/v1"
+)
 
 type CloudProvider interface {
 	Sync(ctx context.Context) error
 	Cleanup(tx context.Context) error
+	Expansion
+}
+
+type Expansion interface {
+	PreCheck(ctx context.Context) (bool, error)
+	GetKubeConfig(ctx context.Context, clusterName string) (string, error)
+	GetCertification(ctx context.Context, clusterName string) ([]v1.Certification, error)
 }
