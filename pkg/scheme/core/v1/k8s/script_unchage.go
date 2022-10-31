35c538e2f74a94ca55668485a1c7bc7820fcdc42
zhuzhenfan <981189503@qq.com>
2022-10-31 08:16:25 +0800

    fix: enable ipv6 forwarding when creating a cluster
    Signed-off-by: zhuzhenfan <981189503@qq.com>

--- pkg/scheme/core/v1/k8s/script_unchage.go
+++ pkg/scheme/core/v1/k8s/script_unchage.go
@@ -13,6 +13,7 @@ cat > /etc/sysctl.d/k8s.conf << EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
 EOF
 sysctl --system
 sysctl -p
