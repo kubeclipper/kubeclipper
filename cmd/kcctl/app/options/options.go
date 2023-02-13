3192217b544a344d078fb5d811eeec84ba8e0751
qinyer <qy913726062@gmail.com>
2023-02-13 16:52:11 +0800

    doc: update ip-detect flag use example
    Signed-off-by: qinyer <qy913726062@gmail.com>

--- cmd/kcctl/app/options/options.go
+++ cmd/kcctl/app/options/options.go
@@ -85,8 +85,34 @@ const (
 	NatsIOClient     = "kc-server-nats-client"
 	NatsIOServer     = "kc-server-nats-server"
 	NatsAltNameProxy = "proxy.kubeclipper.io" // add nats server SAN for agent proxy
-
 )
+
+const IPDetectDescription = `
+To eliminate node specific IP address configuration,the KubeClipper can be configuredto autodetect these IP addresses.
+In many systems, there might be multiple physical interfaces on a host, or possibly multiple IP addresses configured 
+on a physical interface.In these cases, there are multiple addresses to choose from and soautodetection of the correct 
+address can be tricky.
+The IP autodetection methods are provided to improve the selection of thecorrect address, by limiting the selection 
+based on suitable criteria for your deployment.
+The following sections describe the available IP autodetection methods.
+1. first-found
+The first-found option enumerates all interface IP addresses and returns the first valid IP address (based on IP version
+and type of address) on the first valid interface. 
+Certain known “local” interfaces are omitted, such as the docker bridge.The order that both the interfaces and the IP 
+addresses are listed is system dependent.
+This is the default detection method. 
+However, since this method only makes a very simplified guess,it is recommended to either configure the node with a 
+specific IP address,or to use one of the other detection methods.
+2. interface=INTERFACE-REGEX
+The interface method uses the supplied interface regular expression to enumerate matching interfaces and to return the 
+first IP address on the first matching interface. 
+The order that both the interfaces and the IP addresses are listed is system dependent.
+Example with valid IP address on interface eth0, eth1, eth2 etc.:
+interface=eth.*
+3. cidr=CIDR
+The cidr method will select any IP address from the node that falls within the given CIDRs.
+Example:
+cidr=10.0.1.0/24,10.0.2.0/24`
 
 var AssumeYes bool
 
@@ -416,7 +442,7 @@ func getRepoMirror() string {
 
 func (c *DeployConfig) AddFlags(flags *pflag.FlagSet) {
 	flags.StringVarP(&c.Config, "deploy-config", "c", c.Config, "Path to the config file to use for Deploy.")
-	flags.StringVar(&c.IPDetect, "ip-detect", c.IPDetect, "Kc ip detect method.")
+	flags.StringVar(&c.IPDetect, "ip-detect", c.IPDetect, fmt.Sprintf("Kc agent node ip detect method. Used to route between nodes. \n%s", IPDetectDescription))
 	flags.BoolVar(&c.Debug, "debug", c.Debug, "Deploy kc use debug mode")
 	flags.StringVarP(&c.DefaultRegion, "region", "r", c.DefaultRegion, "Kc agent default region")
 	flags.IntVar(&c.ServerPort, "server-port", c.ServerPort, "Kc server port")
