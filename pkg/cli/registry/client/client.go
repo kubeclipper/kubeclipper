6447e823b33260d699d3299344a0e4ac60860944
意琦行 <31980412+lixd@users.noreply.github.com>
2024-06-25 09:23:29 +0800

    fix: registry add insecure options (#706)
    Signed-off-by: lixd <li.xueduan@gmail.com>
    Co-authored-by: lixd <li.xueduan@gmail.com>

--- pkg/cli/registry/client/client.go
+++ pkg/cli/registry/client/client.go
@@ -33,7 +33,7 @@ func Push(file, registryStr string) error {
 			repository := tag.RepositoryStr()
 			target := fmt.Sprintf("%s/%s:%s", registryStr, repository, tag.TagStr())
 			logger.V(2).Infof("[%v/%v] push %s -> %s\n", i+1, len(manifest), t, target)
-			if err = crane.Push(img, target); err != nil {
+			if err = crane.Push(img, target, crane.Insecure); err != nil {
 				return errors.WithMessage(err, "push")
 			}
 
@@ -47,7 +47,7 @@ func Push(file, registryStr string) error {
 				repository = split[1]
 				target = fmt.Sprintf("%s/%s:%s", registryStr, repository, tag.TagStr())
 				logger.V(2).Infof("[%v/%v] push %s -> %s\n", i+1, len(manifest), t, target)
-				if err = crane.Push(img, target); err != nil {
+				if err = crane.Push(img, target, crane.Insecure); err != nil {
 					return errors.WithMessage(err, "push")
 				}
 			}
@@ -59,21 +59,21 @@ func Push(file, registryStr string) error {
 // Delete docker registry doesn't support tag deletion and we have to delete by digest
 // e.g.  crane delete --insecure "localhost:5000/caas4/nfsplugin@$(crane digest localhost:5000/caas4/nfsplugin:v4.1.0)"
 func Delete(registry, repository, tag string) error {
-	digest, err := crane.Digest(fmt.Sprintf("%s/%s:%s", registry, repository, tag))
+	digest, err := crane.Digest(fmt.Sprintf("%s/%s:%s", registry, repository, tag), crane.Insecure)
 	if err != nil {
 		return errors.WithMessage(err, "get digest")
 	}
-	return crane.Delete(fmt.Sprintf("%s/%s@%s", registry, repository, digest))
+	return crane.Delete(fmt.Sprintf("%s/%s@%s", registry, repository, digest), crane.Insecure)
 }
 
 // Catalog return repositories in registry.
 func Catalog(registry string) ([]string, error) {
-	return crane.Catalog(registry)
+	return crane.Catalog(registry, crane.Insecure)
 }
 
 // ListTags return repositories in registry.
 func ListTags(registry, repository string) ([]string, error) {
-	return crane.ListTags(fmt.Sprintf("%s/%s", registry, repository))
+	return crane.ListTags(fmt.Sprintf("%s/%s", registry, repository), crane.Insecure)
 }
 
 func pathOpener(path string) tarball.Opener {
